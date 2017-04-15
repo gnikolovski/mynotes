74386f4e9bb28135174cb4094c4afd0d2bdb4519
Goran Nikolovski <nikolovski84@gmail.com>
2017-04-15 12:35:04 +0200

    Update Quick note block

---
+++ src/Plugin/Block/QuickNoteBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\mynotes\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides a 'Quick Note' block.
+ *
+ * @Block(
+ *   id = "quick_note_block",
+ *   admin_label = @Translation("Quick Note block"),
+ * )
+ */
+class QuickNoteBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $form=[];
+    $form['open_modal'] = [
+      '#type' => 'link',
+      '#title' => $this->t('Quick note'),
+      '#url' => Url::fromRoute('mynotes.quick_note'),
+      '#attributes' => [
+        'class' => ['use-ajax', 'button',],
+      ],
+    ];
+    $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
+    return $form;
+  }
+
+}
